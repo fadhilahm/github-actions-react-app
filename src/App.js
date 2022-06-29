7246dccc0dcf8780536401554a4a5b84150ef7f2
fadhilahm <fadhilahmetra@gmail.com>
2022-06-29 09:46:40 +0900

    fix: Create a commit with the correct semantic release notation.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload and remember to always
-          ハローハッピーワールド！！！1.
+          ハローハッピーワールド！！！12.
         </p>
         <a
           className="App-link"
