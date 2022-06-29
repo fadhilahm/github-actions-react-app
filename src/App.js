7de12f93d6078eb646f5fd6f4213c526628d87b8
fadhilahm <fadhilahmetra@gmail.com>
2022-06-29 14:44:30 +0900

    Added a github-actions test to upload test coverage.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload and remember to always
-          ハローハッピーワールド！！！MASTER.
+          ハローハッピーワールド！！！ CODECOV.
         </p>
         <a
           className="App-link"
