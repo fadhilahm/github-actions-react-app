fff31f21419f852ddbff1c098ef3941e9505f14c
fadhilahm <fadhilahmetra@gmail.com>
2022-07-07 11:54:05 +0900

    fix(action): uploaded the forgotten compiled version

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -9001,7 +9001,7 @@ const main = async () => {
     const body = core.getInput("body");
     const assignees = core.getInput("assignees");
 
-    const octokit = new github.Github(token);
+    const octokit = github.getOctokit(token)
 
     const response = await octokit.issues.create({
       ...github.context.repo,
